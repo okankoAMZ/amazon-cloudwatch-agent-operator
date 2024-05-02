6b95887633b67696d3793a70d4bd2ccb9a604b1a
Paramadon <siprmp@amazon.com>
2024-05-02 12:06:29 -0400

    adding

---
+++ integration-tests/terraform/gpu/variables.tf
@@ -0,0 +1,37 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "k8s_version" {
+  type    = string
+  default = "1.29"
+}
+
+variable "test_dir" {
+  type    = string
+  default = "../../gpu"
+}
+
+variable "addon_name" {
+  type    = string
+  default = "amazon-cloudwatch-observability"
+}
+
+variable "addon_version" {
+  type = string
+  default = "v1.5.3-eksbuild.1"
+}
+
+variable "beta" {
+  type    = bool
+  default = false
+}
+
+variable "beta_endpoint" {
+  type    = string
+  default = "https://api.beta.us-west-2.wesley.amazonaws.com"
+}
